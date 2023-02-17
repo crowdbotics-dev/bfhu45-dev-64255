239e27529bc8814f95578188d59ca0cf17e2439d
Crowdbotics <engineering@crowdbotics.com>
2023-02-17 06:49:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bfhu45_dev_64255/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bfhu45_dev_64255;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bfhu45_dev_64255";
+  }
+}
